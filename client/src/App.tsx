0b8f6aa58ffac4fdc7c33b706cd08a32dda6e0ff
nick <nick@pop-os.localdomain>
2022-01-03 16:38:18 -0500

    set up client template

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./App.css";
+
+function App() {
+  const client = new ApolloClient({
+    uri: "http://localhost:3001/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  return <ApolloProvider client={client}>
+    
+  </ApolloProvider>;
+}
+
+export default App;
---
+++ client/src/components/LogList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LogList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LogList
