041a010fd6ad3f4166ff6d8abe814cc5d454d75e
nick <nick@pop-os.localdomain>
2022-01-13 14:39:30 -0500

    add handling for loading data

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card, CardText } from "reactstrap";
+
+function Loading() {
+  return (
+    <Card>
+      <CardText>Loading...</CardText>
+    </Card>
+  );
+}
+
+export default Loading;
--- client/src/components/LogList.tsx
+++ client/src/components/LogList.tsx
@@ -1,22 +1,46 @@
-import { useQuery } from '@apollo/client'
-import React from 'react'
-import { GET_LOGS } from '../graphql/queries'
-import {Card, CardHeader, CardBody} from "reactstrap";
+import { useQuery } from "@apollo/client";
+import React, { useMemo } from "react";
+import { GET_LOGS } from "../graphql/queries";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
+import Log from "./Log";
+import Loading from "./Loading";
+import Error from "./Error";
 
 function LogList() {
-  const getAllLogs = useQuery(GET_LOGS); 
+  const { loading, data, error } = useQuery(GET_LOGS);
+
+  const loadedData = useMemo(() => {
+    if (loading || error) return null;
+    return data;
+  }, [loading, error, data]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error />;
+
+  // console.log(loadedData.logs);
+
   return (
     <div>
       <Card>
         <CardHeader>List of All Logs</CardHeader>
         <CardBody>
-          <pre>
-            {JSON.stringify(getAllLogs.data)}
-          </pre>
+          <ListGroup>
+            {loadedData.logs.map((log: any) => (
+              <ListGroupItem key={log.id}>
+                <Log date={log.date} />
+              </ListGroupItem>
+            ))}
+          </ListGroup>
         </CardBody>
       </Card>
     </div>
-  )
+  );
 }
 
-export default LogList
+export default LogList;
